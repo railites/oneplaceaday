310a680d88bf2d94a30e7c3dcd8b2d58f0bda00b
pardeep <pardeepdhingra.01@gmail.com>
2015-05-12 14:47:43 -0500

    Refactor Post Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,28 @@
 class PostsController < ApplicationController
-
   before_filter :authenticate_user!, :except => [:index, :show, :add_comment]
+  before_filter :get_post, except: [:index, :create]
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.create(permit_params)
+    @post = Post.create(post_params)
     if @post.save
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
   end
 
   def edit
-    @post = Post.find params[:id]
   end
 
   def update
-    @post = Post.find params[:id]
-    if @post.update(permit_params)
+    if @post.update(post_params)
       redirect_to posts_path
     else
-      render 'edit'
+      render :edit
     end
   end
 
@@ -34,18 +32,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find params[:id]
   end
 
   def add_comment
-    post = Post.find(params[:id])
     post.comments.create(user_id: params[:comment][:user_id], thought: params[:comment][:thought])
     redirect_to post_path(post.id)
   end
 
   private
 
-  def permit_params
+  def post_params
     params.require(:post).permit(:id, :title, :description, :country, :place, :visited_on, :photo, :user_id)
+  end
+
+  def get_post
+    @post = Post.find params[:id]
   end
 end
